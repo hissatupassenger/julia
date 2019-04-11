259bf698ff7f1825b8bd59318148df552b763c90
Keno Fischer <keno@juliacomputing.com>
2019-04-10 22:14:19 -0400

    Fix up jl_obvious_subtype for tuples with bound vararg

--- src/builtins.c
+++ src/builtins.c
@@ -754,7 +754,7 @@ static jl_value_t *get_fieldtype(jl_value_t *t, jl_value_t *f, int dothrow)
         }
         int nf = jl_field_count(st);
         if (nf > 0 && field_index >= nf-1 && st->name == jl_tuple_typename) {
-            jl_value_t *ft = jl_field_type(st, nf-1);
+            jl_value_t *ft = jl_tfield(st, nf-1);
             if (jl_is_vararg_type(ft))
                 return jl_unwrap_vararg(ft);
         }
